e434638a797f3ed11f192087cea9ff778b9053e0
Zhenqi Wu <wzq7799@gmail.com>
2024-01-29 00:28:18 -0700

    new version

--- SVM_classifier.py
+++ SVM_classifier.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import torchvision
 
 class MultiClassSVM(nn.Module):
     def __init__(self, input_size, num_classes):
@@ -18,4 +19,53 @@ def hinge_loss(outputs, labels):
     margins.scatter_(1, correct_indices, 0)
     loss = margins.mean()
     return loss
+
+def train(
+    model,
+    train_loader,    
+    ):
+    for batch_idx, (data, target) in enumerate(train_loader):
+        output = model(data)
+        loss = hinge_loss(output, target)
+        loss.backward()
+        _, argmax = torch.max(output, 1)
+        accuracy = (target == argmax.squeeze()).float().mean()
+        print("accuracy is",accuracy)
+
+def main():
+    svm = MultiClassSVM(input_size=28,num_classes=10)
+    kwargs = {"num_workers": 1, "pin_memory": True} if args.device == "cuda" else {}
+    train_loader = torch.utils.data.DataLoader(
+        torchvision.datasets.MNIST(
+            root=".",
+            train=True,
+            download=True,
+            transform=torchvision.transforms.Compose(
+                [
+                    torchvision.transforms.PILToTensor(),
+                    
+                ]
+            ),
+            
+        ),
+        batch_size=64,
+        shuffle=True,
+        **kwargs,
+    )
+    test_loader = torch.utils.data.DataLoader(
+        torchvision.datasets.MNIST(
+            root=".",
+            train=False,
+            transform=torchvision.transforms.Compose(
+                [
+                    torchvision.transforms.PILToTensor(),
+                    
+                ]
+            ),
+        ),
+        batch_size=64,
+        **kwargs,
+    )
+    train(svm,train_loader)
+
 
