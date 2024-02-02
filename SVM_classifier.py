a611386dee439a3eeed1689dfeab399789424c7d
Zhenqi Wu <wzq7799@gmail.com>
2024-02-01 23:52:05 -0700

    finish svm

--- SVM_classifier.py
+++ SVM_classifier.py
@@ -9,16 +9,19 @@ class MultiClassSVM(nn.Module):
         self.linear = nn.Linear(input_size, num_classes)
 
     def forward(self, x):
-        return self.linear(x)
+        outputs = self.linear(x)
+
+        return outputs
     
 def multiclass_hinge_loss(outputs, labels):
     # 获取类别数
     num_classes = outputs.size(1)
-
     # 正确标签类别的得分
-    correct_scores = outputs[torch.arange(len(labels)), labels].unsqueeze(1)
-
-    # 计算合页损失
+    outputs = outputs.squeeze(1)
+    correct_scores = outputs[torch.arange(len(labels)), labels]
+    #print("correct_scores size",correct_scores.shape)
+    correct_scores = correct_scores.view(len(labels),1)
+    correct_scores = correct_scores.repeat(1,10)
     margins = torch.clamp(1 - (correct_scores - outputs), min=0)
     margins[torch.arange(len(labels)), labels] = 0  # 正确标签的损失设置为0
     loss = margins.sum() / len(labels)
@@ -30,18 +33,20 @@ def train(
     train_loader,    
     ):
     optimizer = optim.SGD(model.parameters(), lr=0.01)
-    for batch_idx, (data, target) in enumerate(train_loader):
+    for batch_idx, (data, label) in enumerate(train_loader):
         optimizer.zero_grad()
+        #print("size of data is",data.shape)
+        data = data.view(data.shape[0],-1)
         output = model(data)
-        loss = multiclass_hinge_loss(output, target)
+        loss = multiclass_hinge_loss(output, label)
         loss.backward()
         optimizer.step()
         _, argmax = torch.max(output, 1)
-        accuracy = (target == argmax.squeeze()).float().mean()
-        print("for epoch {} accuracy is",batch_idx,accuracy)
+        accuracy = (label == argmax.squeeze()).float().mean()
+        print("for epoch {} accuracy is {}".format(batch_idx,accuracy))
 
 def main():
-    svm = MultiClassSVM(input_size=28,num_classes=10)
+    svm = MultiClassSVM(input_size=28*28,num_classes=10)
     train_loader = torch.utils.data.DataLoader(
         torchvision.datasets.MNIST(
             root=".",
