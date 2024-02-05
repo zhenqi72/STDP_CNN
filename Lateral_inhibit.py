e2a3099b84ffa4fb19881f60d2e4c3c4c15c59c7
Zhenqi Wu <wzq7799@gmail.com>
2024-02-04 23:49:56 -0700

    finish inhibit and tested

---
+++ Lateral_inhibit.py
@@ -0,0 +1,32 @@
+import torch     
+
+class Later_inhibt(torch.nn.Module):
+    
+    def __init__(
+        self,
+        input_size,
+    ):
+        super(Later_inhibt, self).__init__()
+        self.input_size = input_size
+        self.mask = torch.ones(input_size)
+        
+        
+    def forward(self, x, mask,m):
+        
+        if m == 0 :
+            mask = mask*x
+            return x, mask
+        else:
+            mask_temp = self.mask*x #create temparl mask to get fired neuron in this time 
+            mask2 = mask_temp + mask # combine with previous mask to see which neuron were add or have to be inhibited
+            result=torch.sum(mask2,1,keepdim=True) #according to the dimesnion 1 which is the channel size to sum all the feature mask
+            position = torch.nonzero(result>1) #if the mask greater than 1, then this position neuron were inhibited
+            dimension0 = position[:,-2]
+            dimension1 = position[:,-1]
+            dimension0=torch.tensor(dimension0)
+            dimension1=torch.tensor(dimension1) #get which neuron should be inhibited
+            mask_temp[:,:,dimension0,dimension1] = 0
+            mask = mask+mask_temp
+            
+            return mask*x, mask
+        
