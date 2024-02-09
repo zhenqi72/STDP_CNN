362e059efd7c3274e786da167359d0b09d770a19
Zhenqi Wu <wzq7799@gmail.com>
2024-02-09 01:14:23 -0700

    new version

--- STDP_selfmade.py
+++ STDP_selfmade.py
@@ -3,10 +3,15 @@ import torch
 
 def get_update_index(v,mask):
     v = v*mask #get the neuron that can fire
-    maxvel,index = torch.max(input=v,dim=1)
-    print("shape of v",v.shape)
-    print("maxvel is",maxvel)
-    return maxvel,index
+    maxvel,index = torch.max(input=v.view(v.shape[0],v.shape[1],-1),dim=2)
+    #print("index is",index)
+    maxind1 = torch.squeeze(index % v.shape[2],0)
+    maxind2 = torch.squeeze(index // v.shape[3],0)
+    #print("shape of v",v.shape)
+    #print("maxvel is",maxvel)
+    #print("maxind1 is",maxind1)
+    #print("maxind2 is",maxind2)
+    return maxvel,maxind1,maxind2
 
 def delta_t(fired,ss):
     d_t = fired*ss
@@ -18,16 +23,22 @@ def STDP_learning(S_pre_sz,s_pre, s_cur, w, threshold,  # Input arrays
     for i in range(w.shape[0]):
         if maxval[i]>threshold:
             # Select the input  
+            input = np.zeros(w[i,:, :, :].shape)
             if maxind2[i]*stride >= S_pre_sz[3] - w.shape[2] and maxind1[i]*stride >= S_pre_sz[2] - w.shape[1]:
-                input = s_pre[0,i,maxind1[i] * stride:, maxind2[i] * stride: ]
+                ss = s_pre[0,:,maxind1[i] * stride:, maxind2[i] * stride:]
+                input[:,:ss.shape[1], :ss.shape[2] ] = ss
             elif maxind2[i]*stride >= S_pre_sz[1] - w.shape[2]:
-                input = s_pre[0,i,maxind1[i] * stride:maxind1[i] * stride + w.shape[0], maxind2[i] * stride:]
+                ss = s_pre[0,:,maxind1[i] * stride:maxind1[i] * stride + w.shape[1], maxind2[i] * stride:]
+                input[:,:, :ss.shape[2]] = ss
             elif maxind1[i]*stride >= S_pre_sz[0] - w.shape[0]:
-                input = s_pre[0,i,maxind1[i] * stride:, maxind2[i] * stride:maxind2[i] * stride + w.shape[2]]
+                ss = s_pre[0,:,maxind1[i] * stride:, maxind2[i] * stride:maxind2[i] * stride + w.shape[2]]
+                input[:,:ss.shape[1], : ] = ss
             else:
-                input = s_pre[0,i,maxind1[i] * stride:maxind1[i]*stride+w.shape[1], maxind2[i]*stride:maxind2[i]*stride+w.shape[2]]
+                input = s_pre[0,:,maxind1[i] * stride:maxind1[i]*stride+w.shape[1], maxind2[i]*stride:maxind2[i]*stride+w.shape[2]]
             # In the paper, it assume that neuron spike either before or after the post-neuron. So, if the 
                 #neuron did input 1 that means it will LTD.
+            #print("input size",input.shape)
+            #print("w size",w.shape)
             dw = input * a_minus * w[i, :, :] * (1 - w[i, :, :]) + \
                 input * a_plus * w[i, :, :] * (1 - w[i, :, :]) - \
                 a_minus * w[i, :, :] * (1 - w[i, :, :])
--- STDP_test.py
+++ STDP_test.py
@@ -2,31 +2,40 @@ from STDP_selfmade import STDP_learning,get_update_index,delta_t
 import torch
 import numpy as np
 
-torch.set_printoptions(threshold=torch.inf)
+torch.set_printoptions(threshold=torch.tensor(10000))
 def main():
     mask2  = np.load("mask2.npy")
+    mask2  = mask2[:,:,0:3,0:3]
     s_pre  = np.load("z3.npy")
+    s_pre  = s_pre[:,:,0:6,0:6]
     s_cur  = np.load("z5.npy")
+    s_cur  = s_cur[:,:,0:3,0:3]
     w  = np.load("w2.npy")
+    w = w[:,:,0:3,0:3]
     v5  = np.load("v5.npy")
+    v5 = torch.from_numpy(v5)
+    v5 = v5[:,:,0:3,0:3]
 
     print("mask2 is",mask2)
-    print("s_pre is",s_pre)
-    print("s_cur is",s_cur)
-    print("w is",w)
-    print("v5 is",v5)
+    print("s_pre is",s_pre.shape)
+    print("s_cur is",s_cur.shape)
+    print("w is",w.shape)
+    print("v5 is",v5.shape)
 
 
 
-    v5 = torch.from_numpy(v5)
-    maxvel,index = get_update_index(v5,mask2)#get the neuron in current plane to update weight
-    maxind1 = index[0,:,0]
-    maxind2 = index[0,:,1]
+    
+    maxvel,maxind1,maxind2 = get_update_index(v5,mask2)#get the neuron in current plane to update weight
+    maxvel = torch.squeeze(maxvel,0)
     S_pre_sz = s_pre.shape
 
     w = STDP_learning(S_pre_sz=S_pre_sz,s_pre=s_pre, s_cur=s_cur, w=w, threshold=10,  # Input arrays
                     maxval=maxvel, maxind1=maxind1, maxind2=maxind2,  # Indices
-                    stride=1, a_minus=0.01, a_plus=0.01)
+                    stride=2, a_minus=0.01, a_plus=0.01)
+    
+    np.savetxt("w after.cvs",np.array(w))
+    np.savetxt("s_pre.cvs",np.array(s_pre))
+    np.savetxt("s_cur.cvs",np.array(s_cur))
 
 if __name__ == "__main__":
     main()
