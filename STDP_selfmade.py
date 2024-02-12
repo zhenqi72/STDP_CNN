17d44fef30d78a5d718be1d15a3a10cf5c10f766
Zhenqi Wu <wzq7799@gmail.com>
2024-02-12 00:08:28 -0700

    new version

--- STDP_selfmade.py
+++ STDP_selfmade.py
@@ -23,26 +23,28 @@ def STDP_learning(S_pre_sz,s_pre, s_cur, w, threshold,  # Input arrays
     for i in range(w.shape[0]):
         if maxval[i]>threshold:
             # Select the input  
-            input = np.zeros(w[i,:, :, :].shape)
-            if maxind2[i]*stride >= S_pre_sz[3] - w.shape[2] and maxind1[i]*stride >= S_pre_sz[2] - w.shape[1]:
+            input = torch.zeros(w[i,:, :, :].shape)
+            #ss is 3 dimensions
+            if maxind2[i]*stride >= S_pre_sz[3] - w.shape[3] and maxind1[i]*stride >= S_pre_sz[2] - w.shape[2]:
                 ss = s_pre[0,:,maxind1[i] * stride:, maxind2[i] * stride:]
-                input[:,:ss.shape[1], :ss.shape[2] ] = ss
-            elif maxind2[i]*stride >= S_pre_sz[1] - w.shape[2]:
-                ss = s_pre[0,:,maxind1[i] * stride:maxind1[i] * stride + w.shape[1], maxind2[i] * stride:]
-                input[:,:, :ss.shape[2]] = ss
-            elif maxind1[i]*stride >= S_pre_sz[0] - w.shape[0]:
-                ss = s_pre[0,:,maxind1[i] * stride:, maxind2[i] * stride:maxind2[i] * stride + w.shape[2]]
-                input[:,:ss.shape[1], : ] = ss
+                input[:,:ss.shape[1], :ss.shape[2] ] = torch.tensor(ss)
+            elif maxind2[i]*stride >= S_pre_sz[3] - w.shape[3]:
+                ss = s_pre[0,:,maxind1[i] * stride:maxind1[i] * stride + w.shape[2], maxind2[i] * stride:]
+                input[:,:, :ss.shape[2]] = torch.tensor(ss)
+            elif maxind1[i]*stride >= S_pre_sz[2] - w.shape[2]:
+                ss = s_pre[0,:,maxind1[i] * stride:, maxind2[i] * stride:maxind2[i] * stride + w.shape[3]]
+                input[:,:ss.shape[1], : ] = torch.tensor(ss)
             else:
-                input = s_pre[0,:,maxind1[i] * stride:maxind1[i]*stride+w.shape[1], maxind2[i]*stride:maxind2[i]*stride+w.shape[2]]
+                input = s_pre[0,:,maxind1[i] * stride:maxind1[i]*stride+w.shape[2], maxind2[i]*stride:maxind2[i]*stride+w.shape[3]]
             # In the paper, it assume that neuron spike either before or after the post-neuron. So, if the 
                 #neuron did input 1 that means it will LTD.
             #print("input size",input.shape)
-            #print("w size",w.shape)
+            #print("w size",w[i].shape)
+            input = torch.tensor(input)
             dw = input * a_minus * w[i, :, :] * (1 - w[i, :, :]) + \
                 input * a_plus * w[i, :, :] * (1 - w[i, :, :]) - \
                 a_minus * w[i, :, :] * (1 - w[i, :, :])
 
             w[i,:] = w[i,:]+dw
     
-    return w
+    return torch.tensor(w)
--- STDP_test.py
+++ STDP_test.py
@@ -1,41 +1,43 @@
-from STDP_selfmade import STDP_learning,get_update_index,delta_t
+from STDP_selfmade import STDP_learning,get_update_index
 import torch
 import numpy as np
 
 torch.set_printoptions(threshold=torch.tensor(10000))
 def main():
+
+    #mask2 is (1, 20, 3, 3)
+    #s_pre is (1, 4, 6, 6)
+    #s_cur is (1, 20, 3, 3)
     mask2  = np.load("mask2.npy")
-    mask2  = mask2[:,:,0:3,0:3]
     s_pre  = np.load("z3.npy")
-    s_pre  = s_pre[:,:,0:6,0:6]
     s_cur  = np.load("z5.npy")
-    s_cur  = s_cur[:,:,0:3,0:3]
+    
     w  = np.load("w2.npy")
-    w = w[:,:,0:3,0:3]
+    w  = torch.tensor(w)
     v5  = np.load("v5.npy")
     v5 = torch.from_numpy(v5)
-    v5 = v5[:,:,0:3,0:3]
-
+    
     print("mask2 is",mask2)
-    print("s_pre is",s_pre.shape)
-    print("s_cur is",s_cur.shape)
-    print("w is",w.shape)
-    print("v5 is",v5.shape)
-
-
-
+    print("s_pre is",s_pre)
+    print("s_cur is",s_cur)
+    #print("w is",w)
+    #print("v5 is",v5)
     
     maxvel,maxind1,maxind2 = get_update_index(v5,mask2)#get the neuron in current plane to update weight
+    print("maxind1",maxind1)
+    print("maxind2",maxind2)
     maxvel = torch.squeeze(maxvel,0)
     S_pre_sz = s_pre.shape
 
-    w = STDP_learning(S_pre_sz=S_pre_sz,s_pre=s_pre, s_cur=s_cur, w=w, threshold=10,  # Input arrays
+    w_after = STDP_learning(S_pre_sz=S_pre_sz,s_pre=s_pre, s_cur=s_cur, w=w, threshold=10,  # Input arrays
                     maxval=maxvel, maxind1=maxind1, maxind2=maxind2,  # Indices
                     stride=2, a_minus=0.01, a_plus=0.01)
-    
-    np.savetxt("w after.cvs",np.array(w))
-    np.savetxt("s_pre.cvs",np.array(s_pre))
-    np.savetxt("s_cur.cvs",np.array(s_cur))
+    #print("w after",w)
+    #np.savetxt("w after.cvs",np.array(w))
+    #np.savetxt("s_pre.cvs",np.array(s_pre))
+    #np.savetxt("s_cur.cvs",np.array(s_cur))
+    not_equal_positions = torch.where(w_after != w)
+    print("不同元素的位置:", not_equal_positions)
 
 if __name__ == "__main__":
     main()
